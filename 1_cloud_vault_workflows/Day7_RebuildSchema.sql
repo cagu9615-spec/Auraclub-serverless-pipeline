0cc613242d6b23b5d4fda698f61667cc87752921
Agu Miracle Chinonye <cagu9615@gmail.com>
2026-08-05 19:05:58 +0100

    Add SQL script to create Members and Revenue tables

---
+++ 1_cloud_vault_workflows/Day7_RebuildSchema.sql
@@ -0,0 +1,20 @@
+-- 1. Create the Members table with multi-field clustering
+CREATE TABLE `my-project-12345-503409.auraclub_analytics.members_staging` (
+  signup_timestamp STRING,
+  full_name STRING,
+  country STRING,
+  membership_type STRING,
+  status STRING
+)
+CLUSTER BY status, membership_type;
+
+-- 2. Create the Revenue table with DAY Partitioning, Clustering, and Native ARRAY Support
+CREATE TABLE `my-project-12345-503409.auraclub_analytics.revenue_staging` (
+  email STRING,
+  source_platform STRING,
+  amount_paid FLOAT64,
+  transaction_date DATE,
+  products_purchased ARRAY<STRING>
+)
+PARTITION BY transaction_date
+CLUSTER BY source_platform;
